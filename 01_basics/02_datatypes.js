c34810bcfd79df616a40e3cc8c1468a549274aa4
FAISAL-imtiyaz <120841156+FAISAL-imtiyaz@users.noreply.github.com>
2024-04-23 11:44:01 +0000

    conversion of datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -13,3 +13,18 @@
 console.log(typeof undefined);  //undefined
 console.log(typeof null) // object
 
+// "33"   33
+// "33abc" NaN
+// true =>1   false =>0
+let isLoggedin=1
+let booleanIsLoggedIn=Boolean(isLoggedin)
+console.log(booleanIsLoggedIn);
+
+// 1=>true  ;0 => false
+// "" =>false
+// "faisal" =>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
