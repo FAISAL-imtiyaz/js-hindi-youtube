a013d0f03f46d3782d65dddf9424d701758b291a
FAISAL-imtiyaz <120841156+FAISAL-imtiyaz@users.noreply.github.com>
2024-04-23 11:27:54 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+// alert(3+1)   we are using node js not browser
+// primitive datatypes
+// Number=> 2 to power 53
+// BigInt
+// String  ""
+// Boolean
+// null=> stand alone value 
+// undefined => value not asigned
+// Symbol
+
+
+// object
+console.log(typeof undefined);  //undefined
+console.log(typeof null) // object
+
