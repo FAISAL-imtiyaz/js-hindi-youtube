eccd8c4cb871624ef10db2d94f5d3045009301f8
FAISAL-imtiyaz <120841156+FAISAL-imtiyaz@users.noreply.github.com>
2024-04-24 02:38:55 +0000

    datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,7 @@
+console.log(null >0);//false
+console.log(null == 0);//false
+console.log(null >= 0);//true
+
+// si,ilarly for undefinded > ,< , ==   =>false in all cases
+// strict check =>  ==='
+console.log("2"===2);
---
+++ 01_basics/datatype summary.js
@@ -0,0 +1,41 @@
+// primitive 
+// 7 types
+// 1>string
+// 2>Number
+// 3> null
+// 4>undefined
+// 5>Symbol
+// 6>BigInt
+// 7>Boolean
+
+// Reference(Non primitive )datatypes        => satatypes for non prim is functions
+//Arrray, Objects,Functions
+
+// Javascript has dynamically typed languageand from
+//  one point of view, it is good because it’s pretty simple
+//   to assign values to the variables. You don’t need to care
+//    about the type of the variable because the type for each
+//  variable will be set dynamically during code execution.
+
+const heros=["shtiman","doga"]
+let myobj={
+    name:"faisal",
+    age:22,
+}
+
+const myfunction= function(){
+    console.log("hello")
+}
+
+
+//   type of val                        RESULT
+//   UNDIFINED                          "undefined"
+//   null                               "object"
+//   BOOLEAN                            "boolean"
+//   Number                              "number"
+//   string                              "string"
+//   Object                              "object"
+//   Object                              "function"
+//   Object                              impl defined may not be "undefined"
+//                                       " boolean","number",or"string"
+
