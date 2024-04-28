f84a5ff716e21491d56625c8eec4a443a3d247b1
FAISAL-imtiyaz <120841156+FAISAL-imtiyaz@users.noreply.github.com>
2024-04-28 20:47:24 +0000

    iife

---
+++ 03_basics/03_iife.js
@@ -0,0 +1,21 @@
+//1
+function chai (){
+    console.log(`DB CONNECTED`);
+}
+chai();
+//2
+(function chai (){
+    //named iife
+    console.log(`DB CONNECTED`);
+})();
+
+// 3
+(() => {
+    //un named iife
+    console.log(`DB CONNECTED TWO `);
+})();
+//4
+((name) => {
+    //parameter  iife
+    console.log(`DB CONNECTED TWO ${name}`);
+})('faisal')
